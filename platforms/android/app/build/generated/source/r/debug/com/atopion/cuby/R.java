b08e1271bd73ec926845667aba5b530bbcf30e26
Timon Vogt <timon.vogt@login.physik.uni-goettingen.de>
2019-07-02 19:32:39 +0200

    New build and removed NFC Button

--- platforms/android/app/build/generated/source/r/debug/com/atopion/cuby/R.java
+++ platforms/android/app/build/generated/source/r/debug/com/atopion/cuby/R.java
@@ -2334,13 +2334,19 @@ public final class R {
     public static final int abc_toolbar_collapse_description=0x7f0c001e;
     public static final int activity_name=0x7f0c001f;
     public static final int app_name=0x7f0c0020;
-    public static final int launcher_name=0x7f0c0021;
-    public static final int search_menu_title=0x7f0c0022;
-    public static final int status_bar_notification_info_overflow=0x7f0c0023;
-    public static final int zxing_app_name=0x7f0c0024;
-    public static final int zxing_button_ok=0x7f0c0025;
-    public static final int zxing_msg_camera_framework_bug=0x7f0c0026;
-    public static final int zxing_msg_default_status=0x7f0c0027;
+    public static final int ionic_app_id=0x7f0c0021;
+    public static final int ionic_channel_name=0x7f0c0022;
+    public static final int ionic_max_versions=0x7f0c0023;
+    public static final int ionic_min_background_duration=0x7f0c0024;
+    public static final int ionic_update_api=0x7f0c0025;
+    public static final int ionic_update_method=0x7f0c0026;
+    public static final int launcher_name=0x7f0c0027;
+    public static final int search_menu_title=0x7f0c0028;
+    public static final int status_bar_notification_info_overflow=0x7f0c0029;
+    public static final int zxing_app_name=0x7f0c002a;
+    public static final int zxing_button_ok=0x7f0c002b;
+    public static final int zxing_msg_camera_framework_bug=0x7f0c002c;
+    public static final int zxing_msg_default_status=0x7f0c002d;
   }
   public static final class style {
     public static final int AlertDialog_AppCompat=0x7f0d0000;
--- platforms/android/app/build/generated/source/r/debug/com/google/zxing/client/android/R.java
+++ platforms/android/app/build/generated/source/r/debug/com/google/zxing/client/android/R.java
@@ -54,10 +54,10 @@ public final class R {
         public static final int zxing_beep = 0x7f0b0000;
     }
     public static final class string {
-        public static final int zxing_app_name = 0x7f0c0024;
-        public static final int zxing_button_ok = 0x7f0c0025;
-        public static final int zxing_msg_camera_framework_bug = 0x7f0c0026;
-        public static final int zxing_msg_default_status = 0x7f0c0027;
+        public static final int zxing_app_name = 0x7f0c002a;
+        public static final int zxing_button_ok = 0x7f0c002b;
+        public static final int zxing_msg_camera_framework_bug = 0x7f0c002c;
+        public static final int zxing_msg_default_status = 0x7f0c002d;
     }
     public static final class style {
         public static final int zxing_CaptureTheme = 0x7f0d015f;
